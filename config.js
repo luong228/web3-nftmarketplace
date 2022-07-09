4c67c8fe676b574330911c1a80346f5ff944c7ca
luong228 <90311663+luong228@users.noreply.github.com>
2022-07-09 14:30:27 +0700

    deploy success

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0xe78B6FF1760A8ADddc1915075E7a55CBfCee392D";
-export const nftaddress = "0x0af3A093b1C75bD025FD1C293bDC7B967a9f67b0";
+export const nftmarketaddress = "0x07a95E264AB6689A23492b316f61BC241Bfc4bb8";
+export const nftaddress = "0x42bB3cfd86B4CE7aB3Bf7334357b7D033D718dBd";
